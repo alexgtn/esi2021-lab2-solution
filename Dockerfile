fe7469aa66f2ec2962673ab80fc1d7a1c1f503f7
alex <alex.gutan.m@gmail.com>
2021-02-14 14:18:30 +0200

    working example

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM golang:1.15.6 as builder
 WORKDIR /app
 COPY . .
-EXPOSE 8080
+EXPOSE 8090
 ENTRYPOINT ["go", "run", "main.go"]
--- main.go
+++ main.go
@@ -9,9 +9,15 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8090", nil)
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+	fmt.Sprintf("req path: %s", req.URL.String())
 	fmt.Fprintf(w, "hello\n")
 }
